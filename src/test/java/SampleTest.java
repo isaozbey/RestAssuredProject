fc5bc3c24cb74debdd6af2bd8d187d0a6e8af0af
isaozbey <isaozbey18@gmail.com>
2020-08-29 19:51:31 -0400

    Test1

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+    @Test
+    public void calculatorTest(){
+
+        assertEquals(9,4+5);
+
+    }
+}
